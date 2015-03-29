f162e7c1d691def0a85f110f47cd3aff57359a80
Vitaly Pryakhin <v.pryakhin@gmail.com>
2015-03-29 03:28:27 +0300

    Third commit

--- qamail.rb
+++ qamail.rb
@@ -1,1 +1,23 @@
 require './base.rb'
+
+get '/static/:file' do
+  send_file('./static/'+params[:file])
+end
+
+get '/favicon.ico' do
+  send_file('./static/favicon.ico')
+end
+
+get '/show_mailbox' do
+  session = Session.where(:session_key => params[:session_key]).first
+  if session == nil then status 404
+  else
+    mailbox = session.mailboxes.where(:address => params[:address]).first
+    if mailbox == nil then status 404
+    else
+      @letters = Letter.where(:mailbox_id => mailbox.id).order(written_at: :desc)
+      @session_key = params[:session_key]
+      erb :show_mailbox
+    end
+  end 
+end
