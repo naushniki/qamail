360bf826be9055cf29ec2e38a27190926674a834
Vitaly Pryakhin <v.pryakhin@gmail.com>
2015-05-10 02:34:09 +0300

    asyncronous notifications

--- base.rb
+++ base.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra/streaming'
 require 'yaml'
 require './models.rb'
 require 'mail'
@@ -7,6 +8,7 @@ require 'digest/sha1'
 
 class QAMail < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  helpers Sinatra::Streaming
 end
 
 $settings = YAML.load_file("settings.yml")
