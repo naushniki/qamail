092c06557cf2e4eb10b4b6d0dec5b71d0d2a97c1
Vitaly Pryakhin <v.pryakhin@gmail.com>
2015-05-11 02:49:51 +0300

    Asynchronous notifications now work

--- notify.rb
+++ notify.rb
@@ -4,21 +4,20 @@ end
 
 streams = Array.new
 
-get '/listen_to_mailbox' do
+get '/listen_to_mailbox', provides: 'text/event-stream' do
   @mailbox = Session.where(:session_key => params[:session_key]).first.mailboxes.where(:address => params[:address]).first
   if @mailbox == nil then
     status 404
     erb :oops
   else 
     stream do |out|
-      out.puts 'HELLO'
       ActiveRecord::Base.connection_pool.with_connection do |connection|
         pg_listen_connection = connection.instance_variable_get(:@connection)
         listen_query = 'listen "' +  @mailbox.address.to_s + '"'
         pg_listen_connection.exec(listen_query)
         while true
           pg_listen_connection.wait_for_notify
-          out.puts 'new'
+          out.puts "data: new letter\n\n"
         end
       end
     end
