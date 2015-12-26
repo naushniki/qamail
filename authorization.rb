02aeb9c35d71a95d45c8ca508932aeb63f03006f
Vitaly Pryakhin <v.pryakhin@gmail.com>
2015-12-26 05:54:13 +0300

    Syntax beautification

--- authorization.rb
+++ authorization.rb
@@ -1,9 +1,9 @@
-def user_session(request, params)
-  session = Session.find_by(:session_key => params[:session_key])
+def user_session
+  session = Session.find_by(:session_key => self.params[:session_key])
   if session != nil
     return session
   else
-    session = Session.find_by(:session_key => request.cookies['session_key'])
+    session = Session.find_by(:session_key => self.request.cookies['session_key'])
     return session
   end
 end
