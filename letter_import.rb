6f34d44dc7cbe2530bda1c87b5935524029a8337
Vitaly Pryakhin <v.pryakhin@gmail.com>
2016-10-18 01:57:18 +0300

    If we fail to parse a message, we must mark it as bad and continue

--- letter_import.rb
+++ letter_import.rb
@@ -13,42 +13,48 @@ import_thread = Thread.new do
   Dir.chdir(dir)
   while(1 == 1) do
     Dir.foreach(".") do |file|
-      next if file.start_with?(".")
-      log.info("Found new letter file: #{file}")
-      parsed_letter = Mail.read(file)
-      attachments_info = String.new
-      if parsed_letter.attachments.count > 0
-        log.info("This letter has attachments")
-        attachments_info << "\nThis letter originally had attachments:\n"
-        parsed_letter.attachments.each do |a|
-          attachments_info << a.filename + "\n"
+      begin
+        next if (file.start_with?(".") or file.end_with?(".bad"))
+        log.info("Found new letter file: #{file}")
+        parsed_letter = Mail.read(file)
+        attachments_info = String.new
+        if parsed_letter.attachments.count > 0
+          log.info("This letter has attachments")
+          attachments_info << "\nThis letter originally had attachments:\n"
+          parsed_letter.attachments.each do |a|
+            attachments_info << a.filename + "\n"
+          end
         end
-      end
-      raw = parsed_letter.without_attachments!.to_s + attachments_info
-      to_address = parsed_letter.header['X-Original-To'].value
-      mailbox = Mailbox.where('lower(address) = ?', to_address.downcase).first
-      if mailbox == nil
-        log.info("Mailbox not found in the database: #{to_address}. This letter was not imported. Deleting file. #{file}")
+        raw = parsed_letter.without_attachments!.to_s + attachments_info
+        to_address = parsed_letter.header['X-Original-To'].value
+        mailbox = Mailbox.where('lower(address) = ?', to_address.downcase).first
+        if mailbox == nil
+          log.info("Mailbox not found in the database: #{to_address}. This letter was not imported. Deleting file. #{file}")
+          File.delete(file)
+          break
+        end
+        subject = parsed_letter.subject
+        from = parsed_letter.from.first
+        written_at = parsed_letter.date
+        letter = Letter.new
+        letter.raw = raw
+        letter.mailbox_id = mailbox.id
+        letter.from = from
+        letter.written_at = written_at
+        letter.subject = subject
+        letter.save
+        log.info("Letter subject is #{letter.subject}")
+        log.info("This letter is for #{mailbox.address}")
+        log.info("Letter imported. Deleting file #{file}")
         File.delete(file)
-        break
+        notify_query = 'notify "' + mailbox.address + '"'
+        log.info("Executing notify query: #{notify_query}")
+        ActiveRecord::Base.connection.execute(notify_query)
+      rescue Exception => e
+        log.error("Failed to parse file #{file} : #{e.message}")
+        log.info("Marking file #{file} as .bad")
+        File.rename(file, file+".bad")
       end
-      subject = parsed_letter.subject
-      from = parsed_letter.from.first
-      written_at = parsed_letter.date
-      letter = Letter.new
-      letter.raw = raw
-      letter.mailbox_id = mailbox.id
-      letter.from = from
-      letter.written_at = written_at
-      letter.subject = subject
-      letter.save
-      log.info("Letter subject is #{letter.subject}")
-      log.info("This letter is for #{mailbox.address}")
-      log.info("Letter imported. Deleting file #{file}")
-      File.delete(file)
-      notify_query = 'notify "' + mailbox.address + '"'
-      log.info("Executing notify query: #{notify_query}")
-      ActiveRecord::Base.connection.execute(notify_query)
     end
     sleep(0.1)
   end
